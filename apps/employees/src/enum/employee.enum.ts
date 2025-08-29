9feb53faf145f58bccb67212a910f8659205a752
Nikhil Babu <nikhilbabu80942@gmail.com>
2025-08-29 10:31:15 +0530

    employee & manager schema

---
+++ apps/employees/src/enum/employee.enum.ts
@@ -0,0 +1,14 @@
+export enum EmployeeStatus {
+  ACTIVE = 'Active',
+  INACTIVE = 'Inactive',
+}
+export enum Gender {
+  MALE = 'Male',
+  FEMALE = 'Female',
+  OTHERS = 'Others',
+}
+
+export enum PreferredPaymentMethod {
+  BANK_ACCOUNT = 'BANK_ACCOUNT',
+  UPI = 'UPI',
+}
---
+++ apps/employees/src/schema/employee.schema.ts
@@ -0,0 +1,139 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { Document } from 'mongoose';
+import {
+  Gender,
+  EmployeeStatus,
+  PreferredPaymentMethod,
+} from '../enum/employee.enum';
+
+@Schema({ _id: false })
+export class BankAccountDetails {
+  @Prop({ type: String, required: false })
+  accountHolderName: string;
+
+  @Prop({ type: String, required: false })
+  accountNumber: number;
+
+  @Prop({ type: String, required: false })
+  ifscCode: string;
+
+  @Prop({ type: String, required: false })
+  bankName: string;
+
+  @Prop({ type: String, required: false })
+  branchName: string;
+}
+
+export const BankAccountDetailsSchema =
+  SchemaFactory.createForClass(BankAccountDetails);
+
+@Schema({ _id: false })
+export class UpiDetails {
+  @Prop({ type: String, required: true })
+  upiId: string;
+
+  @Prop({ type: String, required: false })
+  upiApp: string; // e.g., Google Pay, PhonePe, Paytm
+}
+
+export const UpiDetailsSchema = SchemaFactory.createForClass(UpiDetails);
+
+@Schema({ _id: false })
+export class AccountDetails {
+  @Prop({ type: BankAccountDetailsSchema, required: false })
+  bankAccount: BankAccountDetails;
+
+  @Prop({ type: UpiDetailsSchema, required: false })
+  upi: UpiDetails;
+
+  @Prop({
+    type: String,
+    required: false,
+    enum: PreferredPaymentMethod,
+    default: PreferredPaymentMethod.BANK_ACCOUNT,
+  })
+  preferredPaymentMethod: PreferredPaymentMethod;
+}
+
+export const AccountDetailsSchema =
+  SchemaFactory.createForClass(AccountDetails);
+
+@Schema({ timestamps: true })
+export class Manager extends Document {
+  @Prop({ type: String, unique: true })
+  managerId: string;
+
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, required: true, unique: true })
+  email: string;
+
+  @Prop({ type: String, required: true })
+  contactNumber: string;
+
+  @Prop({ type: String, required: true })
+  password: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, required: true })
+  role: mongoose.Types.ObjectId;
+
+  @Prop({ type: Date, required: true, default: Date.now })
+  joinDate: Date;
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: EmployeeStatus,
+    default: EmployeeStatus.ACTIVE,
+  })
+  status: EmployeeStatus;
+
+  @Prop({
+    type: String,
+    required: false,
+    enum: Gender,
+  })
+  gender: Gender;
+
+  @Prop({ type: String, required: false })
+  address: string;
+
+  @Prop({ type: Number, required: true })
+  salary: number;
+
+  @Prop({ type: String, required: false })
+  profilePhoto: string;
+
+  @Prop({ type: String, required: true })
+  aadhaarFrontImage: string;
+
+  @Prop({ type: String, required: true })
+  aadhaarBackImage: string;
+
+  @Prop({
+    type: [{ type: mongoose.Schema.Types.ObjectId, required: true }],
+    default: [],
+  })
+  propertyId: mongoose.Types.ObjectId[];
+
+  @Prop({ type: AccountDetailsSchema, required: false })
+  accountDetails: AccountDetails;
+
+  @Prop({ type: Boolean, default: false })
+  isVerified: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  loginEnabled: boolean;
+
+  @Prop({ type: Boolean, default: false })
+  isDeleted: boolean;
+
+  @Prop({ type: String, default: null })
+  resetPasswordToken: string;
+
+  @Prop({ type: Date, default: null })
+  resetPasswordExpires: Date;
+}
+
+export const ManagerSchema = SchemaFactory.createForClass(Manager);
